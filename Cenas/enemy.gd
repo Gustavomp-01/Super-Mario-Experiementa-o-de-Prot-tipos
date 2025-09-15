77d5dc479eb03e487e975ce86f3280d5ac1a2ef2
LucasDCoelho <darkalp99@gmail.com>
2025-09-15 19:31:25 -0300

    Algumas Mudanças

---
+++ Cenas/enemy.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+class_name Enemy
+
+
+@export var h_speed = 20
+@export var v_speed = 100
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func _process(delta: float) -> void:
+	position.x -= h_speed * delta
+	
+	if !ray_cast_2d.is_colliding():
+		position.y += v_speed * delta
+
+func _die():
+	h_speed = 0
+	v_speed = 0
+	animated_sprite_2d.play("dead")
