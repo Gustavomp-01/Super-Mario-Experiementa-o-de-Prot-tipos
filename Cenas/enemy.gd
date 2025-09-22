9664658b22c0a7867872a6e859450e25cb452561
Gustavo Maciel Pereira <gustavomacielpereira@hotmail.com>
2025-09-22 20:21:54 -0300

    Penultima aula

--- Cenas/enemy.gd
+++ Cenas/enemy.gd
@@ -5,10 +5,10 @@ class_name Enemy
 
 @export var h_speed = 20
 @export var v_speed = 100
-@onready var ray_cast_2d: RayCast2D = $RayCast2D
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var ray_cast_2d = $RayCast2D
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
-func _process(delta: float) -> void:
+func _process(delta) -> void:
 	position.x -= h_speed * delta
 	
 	if !ray_cast_2d.is_colliding():
@@ -18,3 +18,16 @@ func _die():
 	h_speed = 0
 	v_speed = 0
 	animated_sprite_2d.play("dead")
+	
+func _die_from_hit():
+	h_speed = 0
+	v_speed = 0
+	rotation_degrees = 180
+	
+	set_collision_layer_value(3, false)
+	set_collision_mask_value(1, false)
+	
+	var die_tween = get_tree().create_tween()
+	die_tween.tween_property(self, "position", position + Vector2(0, -25), .2)
+	die_tween.chain().tween_property(self, "position", position + Vector2(0, 500), 4)
+	
--- Cenas/player.gd
+++ Cenas/player.gd
@@ -8,6 +8,12 @@ class_name Player
 @export var jump_velocity = -350
 @export var run_speed_damping = 0.5
 
+@export_group("Stomping Enemies")
+@export var min_stomp_degree = 35
+@export var max_stomp_degree = 145
+@export var stomp_y_velocity = -150
+
+var is_dead = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
@@ -31,3 +37,15 @@ func _physics_process(delta):
 	$Animacao.trigger_animation(velocity, direction)
 	
 	move_and_slide()
+
+
+func _on_area_2d_area_entered(area):
+	if not (area is Enemy) or is_dead:
+		return
+	
+	if area is Koopa and (area as Koopa).in_a_shell:
+		(area as Koopa)._on_stomp(global_position)
+		return
+	
+	var angle = rad_to_deg(position.angle_to_point((area as Enemy).position))
+	
